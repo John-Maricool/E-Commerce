47adf14c3de3b729fdcaf1107c03f6fd767ff0fc
JOHN BATISTA <ebukaiikenwa0902@gmail.com>
2021-12-31 01:24:09 -1100

    Still working on Authentication

--- app/src/main/java/com/maricoolsapps/e_commerce/product_ui/EcommerceActivity.kt
+++ app/src/main/java/com/maricoolsapps/e_commerce/product_ui/EcommerceActivity.kt
@@ -3,16 +3,22 @@ package com.maricoolsapps.e_commerce.product_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.maricoolsapps.e_commerce.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EcommerceActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecommerce)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 }
---
+++ app/src/main/java/com/maricoolsapps/e_commerce/product_ui/FavoriteFragment.kt
@@ -0,0 +1,14 @@
+package com.maricoolsapps.e_commerce.product_ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.maricoolsapps.e_commerce.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+
+}
